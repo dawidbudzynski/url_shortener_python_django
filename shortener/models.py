5489156db48abeadde3890d34e64216d275ede8c
Dawid Budzynski <budzynski.dawid@gmail.com>
2019-04-05 20:40:37 +0200

    project created, Urls model created

---
+++ shortener/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Urls(models.Model):
+    url_code = models.SlugField(max_length=6, primary_key=True)
+    full_url = models.URLField()
+    creation_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'URL: {self.full_url}, code: {self.url_code}, created: {self.creation_date}'
